8ed8ab95a3181873cbd9ec5a51738e41394dbe5c
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-10-11 21:40:39 -0300

    up

--- src/components/SlidesWelcome/SlidesWelcome.tsx
+++ src/components/SlidesWelcome/SlidesWelcome.tsx
@@ -4,16 +4,35 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Navigation, Pagination } from 'swiper/modules';
+import React from 'react';
 
 interface ISlidesWelcome {
   id: number;
   src: string;
 }
 
+
+
 const SlidesWelcome = ({slides} : { slides: ISlidesWelcome[] }) => {  
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    const element = document.querySelector('.swiper-wrapper');
+    
+    if(element) {
+      const parent = element.parentNode;
+      const wrapper = document.createElement('div');
+
+      if(parent) {
+        parent.replaceChild(wrapper, element);
+        wrapper.appendChild(element);
+      }
+    }
+  },[]);
+
   return (
-        <>
         <Swiper
+        ref={ref}
         style={{borderRadius: '1.2rem', width:'100%'}}
         modules={[Pagination, Navigation]}
         pagination={{el:'.swiper-pagination', clickable: true}}
@@ -35,7 +54,6 @@ const SlidesWelcome = ({slides} : { slides: ISlidesWelcome[] }) => {
           <button className='swiper-button-next-custom'></button>
           <div className="swiper-pagination"></div>
         </Swiper>
-        </>
   )
 }
 
