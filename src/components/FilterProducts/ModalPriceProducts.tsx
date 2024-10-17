c4ef4e01db1ca6a1e8722c86d5c7d1c18d5c492f
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-10-17 16:19:45 -0300

    a

--- src/components/FilterProducts/ModalPriceProducts.tsx
+++ src/components/FilterProducts/ModalPriceProducts.tsx
@@ -1,37 +1,12 @@
 import { CaretDown } from '@phosphor-icons/react';
 import './ModalPriceProducts.css'
 import React from 'react';
+import RangeSlider from '../utils/RangeSlider';
 
 const ModalPriceProducts = () => {
   const [animation, setAnimation] = React.useState<boolean>(false);
-  const rangeInput = document.querySelectorAll<HTMLInputElement>('.modal-filter-products-ranger-input input');
-  const progress = document.querySelector<HTMLDivElement>('.modal-filter-products-slider');
-  const priceGap = 1000;
-
-  function handleInput(e: InputEvent) {
-    const minVal = parseInt(rangeInput[0].value);
-    const maxVal = parseInt(rangeInput[1].value);
-
-
-    if(maxVal - minVal < priceGap) {
-      if(e.target.className == 'modal-filter-products-ranger-input-min') {
-        rangeInput[0].value = maxVal - priceGap;
-      } else {
-        rangeInput[1].value = minVal + priceGap;
-      }
-    } else {
-      if(progress) {
-        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-      }
-    }
-  }
-
-  React.useEffect(() => {
-    rangeInput.forEach((input: HTMLInputElement) => {
-      input.addEventListener('input', handleInput);
-    });
-  })
+  const [minPrice, setMinPrice] = React.useState<number>(0);
+  const [maxPrice, setMaxPrice] = React.useState<number>(0);
 
   return (
     <div className="modal-filter-products-container">
@@ -43,26 +18,20 @@ const ModalPriceProducts = () => {
             </button>
         </div>
         <div className={`modal-filter-products-wraper-container ${ animation ? 'hidden' : ''}`}>
-          <div className="modal-filter-products-slider">
-            <div className="modal-filter-products-progress"></div>
-          </div>
-          <div className="modal-filter-products-ranger-input">
-            <input type="range" className='modal-filter-products-ranger-input-min' min={0} max={10000} value={2500}/>
-            <input type="range" className='modal-filter-products-ranger-input-max' min={0} max={10000} value={7500}/>
-          </div>
+          <RangeSlider min={0}  max={1000} onChange={({ min, max }) => {setMinPrice(min); setMaxPrice(max);}}/>
           <div className='modal-filter-products-wraper'>
             <div className="modal-price-products-field">
               <div className="modal-price-products-coin">
                 <span>R$</span>
               </div>
-              <input type="number" className='modal-price-products-number' value={'2500'}/>
+              <input type="number" className='modal-price-products-number' value={minPrice}/>
             </div>
             <div className='modal-price-products-divide'>-</div>
             <div className="modal-price-products-field">
               <div className="modal-price-products-coin">
                 <span>R$</span>
               </div>
-              <input type="number" className='modal-price-products-number' value={'7500'}/>
+              <input type="number" className='modal-price-products-number' value={maxPrice}/>
             </div>
           </div>
         </div>
---
+++ src/components/utils/RangeSlider.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import './RangeSlider.css';
+
+const RangeSlider = ({min, max, onChange}: {min: number, max: number, onChange: any}) => {
+
+    const [minVal, setMinVal] = React.useState(min);
+    const [maxVal, setMaxVal] = React.useState(max);
+    const minValRef = React.useRef(min);
+    const maxValRef = React.useRef(max);
+    const range = React.useRef(null);
+  
+    // Convert to percentage
+    const getPercent = React.useCallback( (value: number) => 
+        Math.round(((value - min) / (max - min)) * 100),
+      [min, max]
+    );
+  
+    // Set width of the range to decrease from the left side
+    React.useEffect(() => {
+      const minPercent = getPercent(minVal);
+      const maxPercent = getPercent(maxValRef.current);
+  
+      if (range.current) {
+        range.current.style.left = `${minPercent}%`;
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }, [minVal, getPercent]);
+  
+    // Set width of the range to decrease from the right side
+    React.useEffect(() => {
+      const minPercent = getPercent(minValRef.current);
+      const maxPercent = getPercent(maxVal);
+  
+      if (range.current) {
+        range.current.style.width = `${maxPercent - minPercent}%`;
+      }
+    }, [maxVal, getPercent]);
+  
+    // Get min and max values when their state changes
+    React.useEffect(() => {
+      onChange({ min: minVal, max: maxVal });
+    }, [minVal, maxVal, onChange]);
+  
+    return (
+      <div className="container">
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={minVal}
+          onChange={(event) => {
+            const value = Math.min(Number(event.target.value), maxVal - 1);
+            setMinVal(value);
+            minValRef.current = value;
+          }}
+          className="thumb thumb--left"
+          style={{ zIndex: minVal > max - 100 && "5" }}
+        />
+        <input
+          type="range"
+          min={min}
+          max={max}
+          value={maxVal}
+          onChange={(event) => {
+            const value = Math.max(Number(event.target.value), minVal + 1);
+            setMaxVal(value);
+            maxValRef.current = value;
+          }}
+          className="thumb thumb--right"
+        />
+  
+        <div className="slider">
+          <div className="slider__track" />
+          <div ref={range} className="slider__range" />
+        </div>
+      </div>
+    );
+
+}
+
+export default RangeSlider;
